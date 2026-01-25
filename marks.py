37c15edcb0e6213879751fb981df2cabf8b707fc
Barahma_muhurat <128905344+nIASHANT@users.noreply.github.com>
2026-01-25 12:51:09 +0530

    Add files via upload
    Assignment/
     ├── marks.py
     ├── dictonary.py
     ├── write.py
     ├── sample.txt
     └── screenshots/

---
+++ marks.py
@@ -0,0 +1,18 @@
+# Take score as input
+score = int(input("Enter your score: "))
+
+# Determine grade using if-else statements
+if score >= 90:
+    grade = "A"
+elif score >= 80:
+    grade = "B"
+elif score >= 70:
+    grade = "C"
+elif score >= 60:
+    g
+    rade = "D"
+else:
+    grade = "F"
+
+# Print the grade40
+print(f"Your grade is: {grade}")
